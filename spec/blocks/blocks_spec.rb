e7e9be8046f321fb1761d5e5d159434a52150b3c
Mark Stratmann <mark@stratmann.me.uk>
2016-01-11 19:43:13 +0000

    Test block properties

--- spec/blocks/blocks_spec.rb
+++ spec/blocks/blocks_spec.rb
@@ -29,9 +29,24 @@ describe '/blocks' do
     end
 
     describe 'update existing block' do
-      let(:id) { "7874c9a7-f875-4059-81ba-5f3f6974df76" }
+      let(:block_id) { JSON.parse(call_endpoint(uri, post_request).body)['id']}
+      let(:update_request)  do
+        req = Net::HTTP::Post.new(uri)
+        req.body = {
+            id: id,
+            sic: "abdce1d5-8bcd-4692-af09-e77c0b20b3d3",
+            watchlist_id: "foo",
+            quantity: 888,
+            price: 11.11,
+            purchased: 100
+        }.to_json
+        req.content_type = 'application/json'
+        sign_request(req, credentials)
+        req
+      end
       it 'returns 201' do
-        expect(call_endpoint(uri, post_request).code).to eql "201"
+        result = call_endpoint(uri, update_request)
+        expect(result.code).to eql "201"
       end
     end
 
@@ -61,6 +76,45 @@ describe '/blocks' do
       expect(result.code).to eql "200"
       expect(JSON.parse(result.body).first['sic']).to eql 'abdce1d5-8bcd-4692-af09-e77c0b20b3d3'
     end
+
+  end
+
+  describe 'post /properties' do
+    let(:block_id) { JSON.parse(call_endpoint(uri, post_request).body)['id']}
+    let(:properties_uri) { URI.join("#{ENV['API_ENDPOINT']}/#{ENV['API_STAGE']}/blocks/properties") }
+    let(:properties) {
+      {
+        "foo" => block_id
+      }
+    }
+    let(:properties_post) do
+      req = Net::HTTP::Post.new(properties_uri)
+      req.body = {
+          id: block_id,
+          properties: properties
+      }.to_json
+      req.content_type = 'application/json'
+      sign_request(req, credentials)
+      req
+    end
+    let(:put_request) do
+      req = Net::HTTP::Put.new(uri)
+      req.body = {
+        ids: [block_id]
+      }.to_json
+      req.content_type = 'application/json'
+      sign_request(req, credentials)
+      req
+    end
+
+    it 'will return properties' do
+      put_properties_result = call_endpoint(properties_uri, properties_post)
+      expect(put_properties_result.code).to eql "201"
+      result = call_endpoint(properties_uri, put_request)
+      expect(result.code).to eql "200"
+      expect(JSON.parse(result.body).first['properties']).to eql properties
+    end
+
   end
 
   describe '/delete' do
